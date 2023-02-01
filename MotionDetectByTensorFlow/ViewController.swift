c026fb2a819ddaadd52f3ed7585d7c75742b68f6
Jiwon Nam <jw.nam@kthd.co.kr>
2023-02-01 10:48:49 +0900

    Initial Commit

---
+++ MotionDetectByTensorFlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionDetectByTensorFlow
+//
+//  Created by Jiwon on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
